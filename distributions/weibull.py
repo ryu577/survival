443e23d761656fad0069a3e0572d08f0706e8618
Rohit Pandey <rohitpandey576@gmail.com>
2018-06-08 17:15:44 -0700

    Added documentation to Weibull

--- distributions/weibull.py
+++ distributions/weibull.py
@@ -4,7 +4,15 @@ from misc.sigmoid import *
 from distributions.basemodel import *
 
 class Weibull(Base):
+    '''
+    Methods pertaining to the Weibull distribution.
+    This is currently the only distribution that supports
+    regressing features.
+    '''
     def __init__(self,k=None,lmb=None,ti=None,xi=None):
+        '''
+        Initializes an instance of the Weibull distribution.
+        '''
         if ti is not None:
             self.train_org = ti
             self.train_inorg = xi
@@ -29,43 +37,129 @@ class Weibull(Base):
             self.t = t[t<1.5]
 
     def determine_params(self, k, lmb, params):
+        '''
+        Sets the parameters. Inherited from basemodel
+        args:
+            k: The shape parameter.
+            lmb: The scale parameter.
+            params: An array of shape and scale parameters.
+        '''
         return super(Weibull, self).determine_params(k, lmb, params)
 
     def logpdf(self,x,k,lmb):
+        '''
+        The logarithm of the PDF function.
+        args:
+            x: Value at which to evaluate.
+            k: Shape parameter.
+            lmb: Scale parameter.
+        '''
         with np.errstate(all='ignore'):
             return np.log(k) - k*np.log(lmb) + (k-1)*np.log(x) - (x/lmb)**k
 
     def pdf(self,x,k=-1,lmb=-1,params=None):
+        '''
+        The probability density function of the Weibull.
+        args:
+            x: The value at which to evaluate.
+            k: The shape parameter.
+            lmb: The scale parameter.            
+        '''
         [k,lmb] = self.determine_params(k,lmb,params)
         return k/lmb*(x/lmb)**(k-1)*np.exp(-(x/lmb)**k)
 
     def pdf_grad(self,x,k,lmb):
+        '''
+        The gradient of the PDF.
+        args:
+            x: The value at which to evaluate.
+            k: The shape parameter.
+            lmb: The scale parameter.
+        '''
         delWeibullDelLmb = (1-(x/lmb)**k)*(-k/lmb)*self.pdf(x, k, lmb)
         delWeibullDelK = self.pdf(x, k, lmb)*( (-(x/lmb)**k+1)*np.log(x/lmb) + 1/k)
         return np.array([delWeibullDelK,delWeibullDelLmb])
 
     def cdf(self,t,k=-1,lmb=-1,params=None):
+        '''
+        The cumulative distribution function.
+        args:
+            t: The value at which to evaluate.
+            k: The shape parameter.
+            lmb: The scale parameter.
+        '''
         return 1 - self.survival(t,k,lmb)
 
     def survival(self,t,k=-1,lmb=-1,params=None):
+        '''
+        The survival function of the distribution (1-cdf)
+        args:
+            t: The value at which to evaluate.
+            k: The shape parameter.
+            lmb: The scale parameter.
+        '''
         [k,lmb] = self.determine_params(k,lmb,params)
         return np.exp(-(t/lmb)**k)
 
     def survival_grad(self,x,k,lmb):
+        '''
+        The gradient of the survival function.
+        args:
+            x: The value at which to evaluate.
+            k: The shape parameter.
+            lmb: The scale parameter.
+        '''
         survive = self.survival(x,k,lmb)
         delk = -survive * (x/lmb)**k * np.log(x/lmb)
         dellmb = survive * (x/lmb)**k * (k/lmb)
         return np.array([delk,dellmb])
 
     def logsurvival(self,t,k=-1,lmb=-1,params=None):
+        '''
+        The logarithm of the survival function.
+        args:
+            t: The value at which to evaluate.
+            k: The shape parameter.
+            lmb: The scale parameter.
+            params: An array of shape and scale parameters.
+        '''
         with np.errstate(all='ignore'):
             return -(t/lmb)**k
 
     def hazard(self,x,k,lmb):
+        '''
+        The hazard rate of the Weibull (probability of seeing
+        a sample in next time unit conditional on not having
+        seen a sample until current time).
+        args:
+            x: The value at which to evaluate.
+            k: The shape parameter.
+            lmb: The scale parameter.
+        '''
         [k,lmb] = self.determine_params(k,lmb,params)
         return self.pdf(x,k,lmb)/self.survival(x,k,lmb)
 
     def loglik(self,t,x = np.array([0]),k=0.5,lmb=0.3,W=None,x_samples=None,x_censored=None):
+        '''
+        The loglikelihood function for some observed data. Used to fit parameters to the data.
+        Ability to regress against features optional.
+        args:
+            t: The observed samples.
+            x: The censored samples.
+            k: The starting value of the shape parameter.
+            lmb: The starting value of the scale parameter.
+            W: In case of regressing with features, this
+               is a matrix of the parameters. It transforms
+               the feature space to the shape and scale
+               parameters of the Weibull.
+            x_samples: The matrix of features associated 
+                       with the sampled observations (t).
+                       Should have the same rows as size of t.
+            x_censored: The matrix of features associated
+                        with the censored observations (x).
+                        Should have the same rows as size of x.
+
+        '''
         #
         ## If there are features, calculate gradient of features.
         #
@@ -90,6 +184,27 @@ class Weibull(Base):
             return sum(self.logpdf(t, k, lmb)) + sum(self.logsurvival(x, k, lmb))
 
     def grad(self,t,x= np.array([1e-3]),k=0.5,lmb=0.3,W=None,x_samples=None,x_censored=None):
+        '''
+        The gradient of the loglikelihood function for some observed data. 
+        Used to fit parameters to the data.
+        Ability to regress against features optional.
+        args:
+            t: The observed samples.
+            x: The censored samples.
+            k: The starting value of the shape parameter.
+            lmb: The starting value of the scale parameter.
+            W: In case of regressing with features, this
+               is a matrix of the parameters. It transforms
+               the feature space to the shape and scale
+               parameters of the Weibull.
+            x_samples: The matrix of features associated 
+                       with the sampled observations (t).
+                       Should have the same rows as size of t.
+            x_censored: The matrix of features associated
+                        with the censored observations (x).
+                        Should have the same rows as size of x.
+
+        '''
         #
         ## If there are features, calculate likelihood with the help of features.
         #
@@ -130,12 +245,42 @@ class Weibull(Base):
             return np.array([delk,dellmb])
 
     def get_params(self,W):
+        '''
+        In the case of regressing against features, 
+        we want to ensure that the final features
+        are always positive. For this, we apply 
+        sigmoid functions to the output of what we
+        get from multiplying the parametres matrix
+        with the feature vector.
+        args:
+            W: The parameter matrix.
+        '''
         theta = np.dot(W.T,x[i])
         kappa = softmax(theta[0],6.0)
         lmb = softmax(theta[1],1000.0)
         return np.array([kappa,lmb])
 
     def numerical_grad(self,t,x,k=0.5,lmb=0.3,W=None,x_samples=None,x_censored=None):
+        '''
+        The numericalgradient of the loglikelihood function for some observed data. 
+        Used to validate the analytic gradient.
+        args:
+            t: The observed samples.
+            x: The censored samples.
+            k: The starting value of the shape parameter.
+            lmb: The starting value of the scale parameter.
+            W: In case of regressing with features, this
+               is a matrix of the parameters. It transforms
+               the feature space to the shape and scale
+               parameters of the Weibull.
+            x_samples: The matrix of features associated 
+                       with the sampled observations (t).
+                       Should have the same rows as size of t.
+            x_censored: The matrix of features associated
+                        with the censored observations (x).
+                        Should have the same rows as size of x.
+
+        '''
         eps = 1e-5
         #
         ## If there are features, calculate likelihood with the help of features.
@@ -180,37 +325,17 @@ class Weibull(Base):
         return params
     '''
 
-    def newtonRh(self,numIter=21, params = np.array([.1,100])):
-        for i in range(numIter):
-            directn = self.grad(self.train_org,self.train_inorg,params[0],params[1])
-            if sum(abs(directn)) < 1e-5:
-                print("\nIt took: " + str(i) + " Iterations.\n Gradients - " + str(directn))
-                [self.k, self.lmb] = params
-                self.params = params
-                return params
-            lik = self.loglik(self.train_org,self.train_inorg,params[0],params[1])
-            step = np.linalg.solve(self.hessian(self.train_org,self.train_inorg,params[0],params[1]),directn)
-            params = params - step
-            params2 = params + 1e-9*directn
-            if min(params) < 0:
-                print("Drastic measures")
-                params = params + step # undo the effect of taking the step.
-                for alp1 in [1e-8,1e-7,1e-5,1e-3,1e-2,.1,.5,1.0]:
-                    params1 = params - alp1 * step
-                    if max(params1) > 0:
-                        lik1 = self.loglik(self.train_org,self.train_inorg,params1[0],params1[1])
-                        if(lik1 > lik and np.isfinite(lik1)):
-                            lik = lik1
-                            params2 = params1
-                            scale = alp1
-                params = params2
-            if i%10 == 0:
-                print("Iteration " + str(i) + " ,objective function: " + str(lik) + " \nparams = " + str(params) + " \nGradient = " + str(directn) + "\n##\n\n")
-        [self.k, self.lmb] = params
-        self.params = params
-        return params
-
+    
     def hessian(self,t,x,k=0.5,lmb=0.3):
+        '''
+        The hessian of the log likelihood function.
+        Used by the Newton Raphson method.
+        args:
+            t: The array of observations of arrival times.
+            x: The array of censored observations.
+            k: The shape parameter.
+            lmb: The scale parameter.
+        '''
         n = len(t)
         delksq = -n/k**2 - sum( (t/lmb)**k*np.log(t/lmb)**2 ) - sum( (x/lmb)**k*np.log(x/lmb)**2 )
         dellmbsq = n*k/lmb**2 + ( sum(t**k) + sum(x**k) )*(- k*(k+1)/lmb**(k+2))
@@ -220,17 +345,7 @@ class Weibull(Base):
         hess[1,1] = dellmbsq
         hess[0,1] = hess[1,0] = dellmbk
         return hess
-
-    def numerical_hessian(self,t,x,k=0.5,lmb=0.3):
-        eps = 1e-4
-        delksq = (self.loglik(t,x,k+2*eps,lmb) + self.loglik(t,x,k-2*eps,lmb) - 2*self.loglik(t,x,k,lmb))/4/eps/eps
-        dellmbsq = (self.loglik(t,x,k,lmb+2*eps) + self.loglik(t,x,k,lmb-2*eps) - 2*self.loglik(t,x,k,lmb))/4/eps/eps
-        dellmbk = (self.loglik(t,x,k+eps,lmb+eps) + self.loglik(t,x,k-eps,lmb-eps) - self.loglik(t,x,k+eps,lmb-eps) - self.loglik(t,x,k-eps,lmb+eps))/4/eps/eps
-        hess = np.zeros([2,2])
-        hess[0,0] = delksq
-        hess[1,1] = dellmbsq
-        hess[0,1] = hess[1,0] = dellmbk
-        return hess
+    
 
     def lmbd(self,t,x,k):
         n = len(x)
@@ -252,11 +367,23 @@ class Weibull(Base):
             else:
                 b=c
 
-    def optimal_wait_threshold(self, reboot_cost):
-        return self.lmb ** (self.k / (self.k - 1)) / (reboot_cost * self.k) ** (1 / (self.k - 1))
+    def optimal_wait_threshold(self, intervention_cost):
+        '''
+        Given the cost of plan B (intervention_cost), what
+        is the optimal time we should wait for this distribution?
+        args:
+            intervention_cost: The cost of giving up on this distribution.
+        '''
+        return self.lmb ** (self.k / (self.k - 1)) / (intervention_cost * self.k) ** (1 / (self.k - 1))
 
     def samples(self, size = 1000):
+        '''
+        Generates samples from current Weibull distribution.
+        args:
+            size: The number of samples to be generated.
+        '''
         return exponweib.rvs(a=1,c=self.k,scale=self.lmb,size=size)
+
 
 
 def generate_features(size):
